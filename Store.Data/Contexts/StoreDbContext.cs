7a4b1964d148a5a56e4bca838432a3350bc3de21
MomoYasser0 <myasoo28@gmail.com>
2024-09-23 22:01:47 +0300

    Product Tables & Migration

---
+++ Store.Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data.Entities;
+using System.Reflection;
+
+namespace Store.Data.Contexts
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        DbSet<ProductBrand> ProductBrands { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
