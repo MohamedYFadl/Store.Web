0ca2d3a7142f4dbce8f9988c3c8896383cdf37a2
MomoYasser0 <myasoo28@gmail.com>
2024-09-23 22:56:55 +0300

    Generic Repository

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -7,7 +7,7 @@ namespace Store.Web
 {
     public class Program
     {
-        public static async void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
