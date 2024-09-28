c2b559744c81bf503cc7ba614377a0f4056753f9
MomoYasser0 <myasoo28@gmail.com>
2024-09-28 21:26:22 +0300

    Product Specifications

---
+++ Store.Repository/Specification/ProductSpecs/ProductSpecification.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Repository.Specification.ProductSpecs
+{
+    public class ProductSpecification
+    {
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+
+    }
+}
---
+++ Store.Repository/Specification/ProductSpecs/ProductWithSpecs.cs
@@ -0,0 +1,15 @@
+﻿using Store.Data.Entities;
+
+namespace Store.Repository.Specification.ProductSpecs
+{
+    public class ProductWithSpecs : BaseSpecification<Product>
+    {
+        public ProductWithSpecs(ProductSpecification specs)
+            : base(product => (!specs.BrandId.HasValue || product.BrandId == specs.BrandId.Value) &&
+            (!specs.TypeId.HasValue || product.BrandId == specs.TypeId.Value))
+        {
+            AddInclude(x => x.BrandId);
+            AddInclude(x => x.TypeId);
+        }
+    }
+}
