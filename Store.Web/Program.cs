bdd9230d907d6eb9e77d7f317a34561693f1ce0c
MomoYasser0 <myasoo28@gmail.com>
2024-09-30 23:09:15 +0300

    Configure ApiBehaviorOptions

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,8 +1,10 @@
 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Store.Data.Contexts;
 using Store.Repository.Interfaces;
 using Store.Repository.Repositories;
+using Store.Service.HandleResponses;
 using Store.Service.ProductService;
 using Store.Service.ProductService.Dtos;
 using Store.Web.Helper;
@@ -26,7 +28,23 @@ namespace Store.Web
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddAutoMapper(typeof(ProductProfile));
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                    .Where(model => model.Value?.Errors.Count >0)
+                    .SelectMany(model=>model.Value?.Errors)
+                    .Select(model=>model.ErrorMessage).ToList();
 
+                    var errorResponse = new ValidationErrorResponse { 
+                    Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
