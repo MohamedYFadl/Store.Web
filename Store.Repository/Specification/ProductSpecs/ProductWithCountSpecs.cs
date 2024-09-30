dd03885cb53d6f31190222d59b1ad7e00eda10be
MomoYasser0 <myasoo28@gmail.com>
2024-09-30 22:18:16 +0300

    Add Search To Specifications

--- Store.Repository/Specification/ProductSpecs/ProductWithCountSpecs.cs
+++ Store.Repository/Specification/ProductSpecs/ProductWithCountSpecs.cs
@@ -6,7 +6,8 @@ namespace Store.Repository.Specification.ProductSpecs
     {
         public ProductWithCountSpecs(ProductSpecification specs)
     : base(product => (!specs.BrandId.HasValue || product.BrandId == specs.BrandId.Value) &&
-    (!specs.TypeId.HasValue || product.TypeId == specs.TypeId.Value))
+             (!specs.TypeId.HasValue || product.TypeId == specs.TypeId.Value) &&
+             (string.IsNullOrEmpty(specs.Search) || product.Name.Trim().ToLower().Contains(specs.Search)))
         {
         
         }
--- Store.Repository/Specification/ProductSpecs/ProductWithSpecs.cs
+++ Store.Repository/Specification/ProductSpecs/ProductWithSpecs.cs
@@ -6,7 +6,8 @@ namespace Store.Repository.Specification.ProductSpecs
     {
         public ProductWithSpecs(ProductSpecification specs)
             : base(product => (!specs.BrandId.HasValue || product.BrandId == specs.BrandId.Value) &&
-            (!specs.TypeId.HasValue || product.TypeId == specs.TypeId.Value))
+            (!specs.TypeId.HasValue || product.TypeId == specs.TypeId.Value) && 
+            (string.IsNullOrEmpty(specs.Search) ||  product.Name.Trim().ToLower().Contains(specs.Search)) )
         {
             AddInclude(x => x.Brand);
             AddInclude(x => x.Type);
