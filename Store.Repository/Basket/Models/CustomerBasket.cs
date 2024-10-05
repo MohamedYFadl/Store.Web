488c2228b5aa36cb98d993153257ebf83ac2ddc4
MomoYasser0 <myasoo28@gmail.com>
2024-10-05 15:37:28 +0300

    BasketRepository Setup

---
+++ Store.Repository/Basket/Models/CustomerBasket.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Repository.Basket.Models
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public decimal ShippingPrice { get; set; }
+        public List<BasketItems> BasketItems { get; set; } = new List<BasketItems>();
+    }
+}
